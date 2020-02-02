3842b3a09dfb36509ba2b2ebe43867259212b25a
vincentnqc <vincentnqc92@gmail.com>
2020-02-01 18:59:34 -0600

    getting there

--- src/components/Car.js
+++ src/components/Car.js
@@ -3,6 +3,7 @@ import cars from '../cars.json'
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Chip from '@material-ui/core/Chip';
+import './Car.css'
 // import material ui components here //
 // Container, Paper, Chip //
 
@@ -12,9 +13,25 @@ const Car = (props) => {
     console.log(id, car)
     return (<Container>
         <h1>{car.Name}</h1>
-        <Paper>
+        <div class="box">
+        <Paper style= {
+            {height: 100,
+            width: 500,
+            justifyContent: "center",
+            alignContent: "center",
+        }}>
             <Chip label={`ID:${car.id}`}></Chip>
+            <Chip label={`Name:${car.Name}`}></Chip>
+            <Chip label={`MPG:${car.Miles_per_Gallon}`}></Chip>
+            <Chip label={`Cylinders:${car.Cylinders}`}></Chip>
+            <Chip label={`Displacement:${car.Displacement}`}></Chip>
+            <Chip label={`Horsepower:${car.Horsepower}`}></Chip>
+            <Chip label={`Weight Lbs:${car.Weight_in_lbs}`}></Chip>
+            <Chip label={`Acceleration:${car.Acceleration}`}></Chip>
+            <Chip label={`Year:${car.Year}`}></Chip>
+            <Chip label={`Origin:${car.Origin}`}></Chip>
         </Paper>
+        </div>
         </Container>
     )
 }
